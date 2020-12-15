70e94011a4fca9b798ef07b69ec0dd369385a83a
TimoWieme <70606358+TimoWieme@users.noreply.github.com>
2020-12-15 14:10:52 +0100

    Calendar + Werkende Next/Prev Month

--- calendar.php
+++ calendar.php
@@ -1,29 +1,62 @@
 <?php
 function build_calendar($month, $year){
+    $mysqli = new mysqli('localhost', 'root', '', 'covid-db');
+    $stmt = $mysqli-> prepare("select * from contact where MONTH(date) = ? AND YEAR(date) = ?");
+    $stmt->bind_param('ss', $month, $year);
+    $bookings = array();
+    if($stmt->execute()){
+        $result = $stmt->get_result();
+        if($result->num_rows>0){
+            while ($row = $result->fetch_assoc()){
+                $bookings[] = $row['date'];
+            }
+
+            $stmt->close();
+        }
+    }
+
+    // Een Array met alle dagen van de week
     $daysOfWeek = array('Zondag','Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag');
+
+    // Hiermee bepaal je de eerste dag van de maand
     $firstDayOfMonth = mktime(0,0,0, $month, 1, $year);
+
+    // Hiermee bepaal je de hoeveelheid dagen in een maand.
     $numberDays = date('t',$firstDayOfMonth);
+
+    // Informatie krijgen over de eerste dag van deze maand.
     $dateComponents = getdate($firstDayOfMonth);
+
+    // De naam van deze maand krijgen
     $monthName = $dateComponents['month'];
+
     $dayOfWeek = $dateComponents['wday'];
+
+    // Hiermee krijg ik de datum van vandaag.
     $dateToday = date('Y-m-d');
 
-    $calendar = "<table class='table table-bordered'>";
+    // Het maken van de HTML tabel (dus de kalender)
+    $calendar= "<table class='table table-bordered'>";
 
-    $calendar.="<center><h2>$monthName $year</h2>";
+    $calendar .="<center><h2>$monthName $year</h2>";
 
-    $calendar.="<a class='btn btn-xs btn-primary' href='?month=".date('m', mktime(0,0, 0, $month-1, 1, $year))."&year=".date('Y', mktime(0,0, 0, $month-1, 1, $year))."'>Vorige Maand</a>";
+    // De knoppen om naar de volgende, vorige of huidige maand gaan op de kalender.
+    $calendar.="<a class='btn btn-xs btn-primary' href='?month=".date('m', mktime(0,0, 0, $month-1, 1, $year)).
+        "&year=".date('Y', mktime(0,0, 0, $month-1, 1, $year))."'>Vorige Maand</a>";
 
     $calendar.="<a class='btn btn-xs btn-primary' href='?month=".date('m')."&year=".date('Y')."'>Deze Maand</a>";
 
     $calendar.="<a class='btn btn-xs btn-primary' href='?month=".date('m', mktime(0,0, 0, $month+1, 1, $year))."&year=".date('Y', mktime(0,0, 0, $month+1, 1, $year))."'>Volgende Maand</a></center><br>";
 
     $calendar.="<tr>";
+
+    // Maken van de Headers van de kalender.
     foreach($daysOfWeek as $day){
         $calendar.="<th class='header'>$day</th>";
     }
     $calendar.= "</tr><tr>";
 
+    // Met de variabele $dayOfWeek zorg je dat er maar 7 kolommen zijn in de tabel.
     if($dayOfWeek > 0) {
         for($k=0;$k<$dayOfWeek;$k++){
             $calendar.="<td></td>";
@@ -31,9 +64,12 @@ function build_calendar($month, $year){
     }
 
     $currentDay = 1;
-    $month = str_pad($month, 2, "0", STR_PAD_LEFT);
-    while($currentDay <=$numberDays){
 
+    // Krijgen van het nummer van de maand.
+    $month = str_pad($month, 2, '0', STR_PAD_LEFT);
+    while($currentDay <= $numberDays){
+
+        // Als de 7e kolom (dus zaterdag) is bereikt, start een nieuwe rij.
         if($dayOfWeek == 7){
             $dayOfWeek = 0;
             $calendar.="</tr><tr>";
@@ -45,8 +81,12 @@ function build_calendar($month, $year){
         $dayName = strtolower(date('l', strtotime($date)));
         $eventNum = 0;
         $today = $date==date('Y-m-d')? "today" : "";
+
+        // Als de dag beschikbaar is om te boeken of niet, zal er een groene of rode knop op de dag zitten.
         if($date<date('Y-m-d')){
             $calendar.="<td><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'>Niet Beschikbaar</button>";
+        }elseif(in_array($date, $bookings)){
+            $calendar.="<td class='$today'><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'>Vol Geboekt</button>";
         }else{
             $calendar.="<td class='$today'><h4>$currentDay</h4> <a href='reserveer.php?date=".$date."' class='btn btn-success btn-xs'>Reserveer</a>";
 
@@ -54,10 +94,12 @@ function build_calendar($month, $year){
 
         $calendar.="</td>";
 
+        // Hiermee verhoog ik de tellers.
         $currentDay++;
         $dayOfWeek++;
     }
 
+    // De rij van de laatste week van de maand afmaken, als dit nodig is.
     if($dayOfWeek !=7){
         $remainingDays = 7-$dayOfWeek;
         for($i=0;$i<$remainingDays;$i++){
@@ -77,29 +119,32 @@ function build_calendar($month, $year){
 <head>
     <meta name="viewport" content=""width="device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <style>
-        table{
-            table-layout: fixed;
-        }
-
-        td{
-            width: 33%;
-        }
-
-        .today{
-            background: rgb(52, 194, 189);
-        }
-    </style>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+<header>
+    <div id="logo">
+        <img src="./logocovid.png">
+    </div>
+</header>
+<!--De links voor de navigatiebar-->
+<ul>
+    <li><a href="#home">Coronatest</a></li>
+    <li><a href="#">Mobiel testen</a></li>
+    <li><a href="#contact">Contact</a></li>
+</ul>
+
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <?php
                  $dateComponents = getdate();
-                 $month = $dateComponents['mon'];
-                 $year = $dateComponents['year'];
-                 echo build_calendar($month, $year);
+                 $month = $_GET['month'];
+                 $year = $_GET['year'];
+
+
+
+                echo build_calendar($month, $year);
                 ?>
             </div>
         </div>
