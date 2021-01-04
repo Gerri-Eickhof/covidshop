24f67529a75034aebd8d60d82f6eaa41bfa895cb
TimoWieme <1004149@hr.nl>
2021-01-04 11:30:23 +0100

    Added javascript function, connect js file to reserveer.php

---
+++ Script/script.js
@@ -0,0 +1,4 @@
+function confirmInput() {
+    fname = document.forms[0].firstname.value;
+    alert("Hallo " + fname + "! U zal nu terug gestuurd worden naar de kalender!");
+}
--- reserveer.php
+++ reserveer.php
@@ -1,4 +1,5 @@
 <?php
+
  require_once 'php/db_connection.php'; //connecting the db_connection to this file
  $conn = openCon();
 
@@ -32,6 +33,8 @@ if (isset($_POST['submit'])) {
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <script src="myscripts.js"></script>
+
         <title>Reservering</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
