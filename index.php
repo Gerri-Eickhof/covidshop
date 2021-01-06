7d857cda2bf6afe016d3b46d4b51c4c135909f38
TimoWieme <1004149@hr.nl>
2021-01-06 10:54:41 +0100

    Removed index-old.php and all of index.php to create new page.

--- index.php
+++ index.php
@@ -1,75 +1,13 @@
-<?php
-require_once 'php/db_connection.php'; //connecting the db_connection to this file
-
-$conn = openCon(); //making connection to the database
-
-// making a record for firs-, lastname and email.
-// $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-// VALUES ('Olivier', 'Vromans', 'olivier@vromans.eu')";
-// if ($conn->query($sql) === TRUE) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . $conn->error;
-// }
-
-closeCon($conn); //close connection
-?>
-<html>
+<<!doctype html>
+<html lang="en">
 <head>
-  <link href = 'Styles/style.css' type = "text/css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-  <form action="insert.php" method="post">
-    <table>
-      <tr>
-        <td>Voornaam:</td>
-        <td><input type="text" name="firstname"></td>
-      </tr>
-      <tr>
-        <td>Achternaam:</td>
-        <td><input type="text" name="lastname"></td>
-      </tr>
-      <tr>
-        <td>Email:</td>
-        <td><input type="email" name="email"></td>
-      </tr>
-      <tr>
-        <td>Telefoonnummer:</td>
-        <td><input type="tel" name="phone"></td>
-      </tr>
 
-      <tr>
-        <td>adres:</td>
-        <td><input type="text" name="adress"></td>
-      </tr>
-      <tr>
-        <td>Postcode:</td>
-        <td><input type="text" name="zipcode"></td>
-      </tr>
-      <tr>
-        <td>Plaats:</td>
-        <td><input type="text" name="city"></td>
-      </tr>
-        <tr>
-            <td>Provincie:</td>
-            <td><input type="text" name="state"></td>
-        </tr>
-      <tr>
-        <td>Hoeveelheid testen:</td>
-        <td><input type="number" min="0" name="products"></td>
-      </tr>
-      <tr>
-        <td>Datum:</td>
-        <td><input type="date" name="date"></td>
-      </tr>
-      <tr>
-        <td>Tijd:</td>
-        <td><input type="time" name="time"></td>
-      </tr>
-      <tr>
-        <td>Versturen :</td>
-        <td><input type="submit" name="submit" value="submit"></td>      </tr>
-    </table>
-  </form>
 </body>
 </html>
