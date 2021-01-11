0730c145fe07edd29e0b62a8b7af4e909f5fa221
TimoWieme <1004149@hr.nl>
2021-01-11 14:05:35 +0100

    Removed some not-used Javascript

--- includes/script.js
+++ includes/script.js
@@ -1,9 +1,9 @@
-function confirmInput() {
-    fname = document.forms[0].firstname.value;
-    alert("Hallo " + fname + "! U zal nu terug gestuurd worden naar de kalender!");
-}
-var Process = document.getElementsByName('submit');
-Process.onclick = myFunction;
+// function confirmInput() {
+//     fname = document.forms[0].firstname.value;
+//     alert("Hallo " + fname + "! U zal nu terug gestuurd worden naar de kalender!");
+// }
+// var Process = document.getElementsByName('submit');
+// Process.onclick = myFunction;
 
 
 function openForm() {
