f8b01eb9ea5c36fbd23e59c9e314e30dc6eff5ab
TimoWieme <1004149@hr.nl>
2021-01-11 14:53:07 +0100

    Removed some JS

--- includes/script.js
+++ includes/script.js
@@ -1,12 +1,3 @@
-function confirmInput() {
-    fname = document.forms[0].firstname.value;
-    alert("Hallo " + fname + "! U zal nu terug gestuurd worden naar de kalender!");
-}
-
-
-// function openForm() {
-//     document.getElementById("myForm").style.display = "block";
-// }
 
 document.getElementById("closeForm").onclick = function closeForm() {
     document.getElementById("myForm").style.display = "none";
